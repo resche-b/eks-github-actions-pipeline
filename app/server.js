41fe5d8c5e76d189b74d82e1b9ffdbe9fd90604a
rescheb <resche26@live.com>
2024-12-31 15:34:06 -0500

    testing docker

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello, from EKS project testv3'));
+app.get('/', (req, res) => res.send('Hello, from EKS project testv4'));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
